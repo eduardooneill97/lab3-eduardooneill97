3b97536ddf6809a8a107a07334f75fae4988a5bc
4015c11 <4015c11@mango.ece.uprm.edu>
2017-11-15 16:41:09 -0400

    comment

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,6 +4,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Hello, world!");
 		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		//
 		myFrame.setLocation(200, 200);
 		myFrame.setSize(400, 100);
 		myFrame.setVisible(true);
